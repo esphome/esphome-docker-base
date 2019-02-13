f48df41da72943a8ef63918888e8ae5dbd1f4134
Otto Winter <otto@otto-winter.com>
2019-02-13 18:00:34 +0100

    Gitlab

--- gen.py
+++ gen.py
@@ -6,6 +6,7 @@ hassio_template = root_path / 'template' / 'Dockerfile.hassio'
 docker_template = root_path / 'template' / 'Dockerfile'
 qemu_path = root_path / 'qemu'
 
+
 def copy_hook(arch):
     t = root_path / arch / 'hooks'
     if t.exists():
@@ -13,6 +14,7 @@ def copy_hook(arch):
     if arch in ('amd64', 'i386'):
         return
     shutil.copytree(root_path / 'hooks', t)
+
 
 def gen_hassio(hassio_arch):
     d = root_path / hassio_arch
@@ -24,9 +26,11 @@ def gen_hassio(hassio_arch):
     target.write_text(temp)
     copy_hook(hassio_arch)
 
+
 HASSIO_ARCHS = ['amd64', 'i386', 'armhf', 'aarch64']
 for arch in HASSIO_ARCHS:
     gen_hassio(arch)
+
 
 def gen_docker(target_arch, docker_arch, qemu_arch):
     d = root_path / target_arch
@@ -47,8 +51,7 @@ def gen_docker(target_arch, docker_arch, qemu_arch):
 DOCKER_ARCHS = [
     ('amd64', 'amd64', None),
     ('i386', 'i386', None),
-    # arm32v6 has no python:2.7[-slim], arm32v7 does not work with qemu
-    # ('armhf', 'arm32v6', 'arm'),
+    ('armhf', 'arm32v7', 'arm'),
     ('aarch64', 'arm64v8', 'aarch64')
 ]
 
